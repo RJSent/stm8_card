b710cce98a02389fd141064b7961edd581896921
RJSent <richard.j.sent@tutamail.com>
2021-02-05 09:34:29 -0500

    experimenting with stack overflow

--- main.c
+++ main.c
@@ -4,9 +4,23 @@
 // blink_clear(char port, int *pins)
 // Toggles all pins to low
 
+#include <stdlib.h>
+
 #include "baseline.h"
 #include "blink_code.h"
 #include "uart.h"
+
+
+void second_func(int i) {
+  volatile char char_var = 'c';
+  i -= 1;
+  if (i > 0) {
+    second_func(i);
+  }
+}
+
+
+
 
 int main() {
   const char *pins[] = {"D3", "D2", "D1", "C7", "C6", "C5", "C4", "C3"};
@@ -16,20 +30,20 @@ int main() {
 
   clk_hsi_prescaler(1);
   uart_init(baud_rate, fmaster);
+  int i = 600;
+
+  char* buf = calloc(512, 1);
+  for (int i = 0; i < 512; i++)
+    buf[i] = 'a';
   
   while (1) {
-    uart_printf("Binary:    %b\n\r", 8);
-    uart_printf("Character: %c\n\r", 'c');
-    uart_printf("Signed:    %d\n\r", -14);
-    uart_printf("String:    %s\n\r", "Hi there!");
-    uart_printf("Unsigned:  %u\n\r", 14);
-    uart_printf("Hex:       %x\n\r", 12);
+    /* uart_printf("Binary:    %b\n\r", 8); */
+    /* uart_printf("Character: %c\n\r", 'c'); */
+    /* uart_printf("Signed:    %d\n\r", -14); */
+    /* uart_printf("String:    %s\n\r", "Hi there!"); */
+    /* uart_printf("Unsigned:  %u\n\r", 14); */
+    /* uart_printf("Hex:       %x\n\r", 12); */
 
-    blink_code(random_upto((1 << 8) - 1));
-    delay(4000000);
+    second_func(i);
   }
 }
-
-
-
-
