ee267235ac298ca1c44d8d3d2375d62f6b166edf
RJSent <richard.j.sent@tutamail.com>
2021-02-14 09:41:42 -0500

    some tidying up

--- i2c.c
+++ i2c.c
@@ -10,11 +10,16 @@
 #define TRANSMIT 0
 #define RECEIVE  1
 
-#define TXE  ((I2C_SR1 & 0x80) >> 7)
+#define SB   ((I2C_SR1 & 0x01))
 #define ADDR ((I2C_SR1 & 0x02) >> 1)
 #define BTF  ((I2C_SR1 & 0x04) >> 2)
+#define TXE  ((I2C_SR1 & 0x80) >> 7)
 
-#define TRA  ((I2C_SR3 & 0x04) >> 3)
+#define AF   ((I2C_SR2 & 0x04) >> 2)
+
+#define BUSY ((I2C_SR3 & 0x02) >> 1)
+#define TRA  ((I2C_SR3 & 0x04) >> 2)
+
 
 int i2c_init(uint8_t freq_mhz) {
   uint8_t trise = freq_mhz + 1;      /* pg. 318 in rm0016. I think this shortcut calculation is valid */
@@ -37,7 +42,7 @@ int i2c_send_byte(uint8_t data) {
 
 void i2c_start_condition() {
   I2C_CR2 |= START_GEN;
-  while ((I2C_SR1 & 0x01) != 1) {}; /* return when start condition is sent */
+  while (!SB) {}; /* return when start condition is sent */
 }
 
 void i2c_stop_condition() {
@@ -51,17 +56,17 @@ void i2c_debug() {
   uart_printf("SR3: 0b%b\n\r", I2C_SR3);
 }
 
+/* FIXME: i2c_start_condition() reads SR1, meaning we can't read SR3
+   at all before the if statement as ADDR would be cleared and we'd
+   have a NACK_ERROR even though it was ACK'd */
 int i2c_send_bytes(uint8_t *data, char size, uint8_t addr) {
-  volatile uint8_t temp1, temp2, temp3, temp4, temp5, temp6;
+  volatile uint8_t temp;
   i2c_start_condition();
   i2c_send_byte(addr << 1 | TRANSMIT);
-  /* while (TRA != 1) {}; */
-  /* clear ADDR by reading SR1 and SR3 */
-  delay(30);                    /* fixme hardcoded delay. Need to wait until we get ACK */
-  if (ADDR == 1) {
-    temp6 = I2C_SR3;
-        /* while (BTF == 0) {}; */
-        /* I2C_DR = 1; */
+  delay(30);                    /* FIXME: hardcoded delay. Need to wait until we get ACK, but I can't read SR3 to do it since that'll reset ADDR */
+  /* clear ADDR by reading SR1 then SR3 */
+  if (ADDR == 1) {              /* read SR1 */
+    temp = I2C_SR3;             /* read SR3 */
     for (int i = 0; i < size; i++) {
       i2c_send_byte(data[i]);
       while (TXE != 1) {};
@@ -71,5 +76,4 @@ int i2c_send_bytes(uint8_t *data, char size, uint8_t addr) {
   } else {
     return NACK_ERROR;
   }
-
 }
