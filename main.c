d7b487d497f9b3a9581e2f4af0d31c0179c0e0e6
RJSent <richard.j.sent@tutamail.com>
2021-02-15 12:05:57 -0500

    changed mux ratio + trying to display hello world manually

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main() {
   data[2] = CMD_TIM_DISPLAY_CLK_DIVIDE_RATIO;
   data[3] = 0x80;                /* ratio 0x80 */
   data[4] = CMD_HW_MULTIPLEX_RATIO;
-  data[5] = 0x1F;                /* ratio 32 mux */
+  data[5] = 0x3F;                /* ratio 63 mux */
   data[6] = CMD_HW_COM_PINS_CONFIG;
   data[7] = 0x12;                /* split for 128x32 */
   data[8] = 0x20;   /* set addressing mode */
@@ -63,14 +63,40 @@ int main() {
 
   /* uart_printf("Done!\n\r"); */
 
-  int i = 0;
-
+    uint8_t buffer[16][4] = {   /* hello world attempt, transcribed from gimp manually */
+    {0x00, 0x00, 0x00, 0x00},
+    {0x50, 0x14, 0x00, 0x00},
+    {0x53, 0xB9, 0x40, 0x00},
+    {0x53, 0xB9, 0x40, 0x00},
+    {0x73, 0x96, 0xB0, 0x00},
+    {0x72, 0x16, 0x40, 0x00},
+    {0x73, 0x96, 0xB0, 0x00},
+    {0x00, 0x00, 0x00, 0x00},   /* 2 blank rows */
+    {0x00, 0x00, 0x00, 0x00},
+    {0x40, 0x40, 0x21, 0x00},
+    {0x40, 0x40, 0x21, 0x00},
+    {0x20, 0x80, 0x21, 0x00},
+    {0x11, 0x00, 0x21, 0x00},
+    {0x16, 0x2A, 0x26, 0x00},
+    {0x0A, 0x3A, 0x37, 0x00}
+  };           /* 16 columns of 32 pixels */
+  
   while (1) {
     data[0] = CONTROL_BYTE(CO_DATA, DC_DATA);
-    data[1] = random_upto(0xFF);
-    if (NACK_ERROR == i2c_send_bytes(data, 2, 0x3C)) {
-      uart_printf("err\n\r");
+    for (int i = 0; i < 32; i++) {
+      for (int j = 0; j < 16; j++) {
+	if (j > 3 || i > 15) {
+	  data[1] = 0;
+	} else {
+	  data[1] = buffer[i][j];
+	}
+	i2c_send_bytes(data, 2, 0x3C);
+      }
     }
-    delay(50);
+    /* data[1] = random_upto(0xFF); */
+    /* if (NACK_ERROR == i2c_send_bytes(data, 2, 0x3C)) { */
+      /* uart_printf("err\n\r"); */
+    /* } */
+    delay(500);
   }
 }
