6c229300aa175fc87924f5f1dd4ccb1c91a7234d
RJSent <richard.j.sent@tutamail.com>
2021-02-27 19:37:29 -0500

    work on game structs

--- space_invader.c
+++ space_invader.c
@@ -2,8 +2,21 @@
 
 #include "space_invader.h"
 
+/* TODO: Refactor code so that .state is a part of the PlayerLaser and PlayerShip structs */
+
 #define MAX_PLAYER_LASERS 3
 #define MAX_ENEMY_LASERS 3
+
+struct PlayerLaser {
+  boolean_t active;
+  struct DrawableImage laser;
+};
+
+struct PlayerShip {
+  boolean_t alive;
+  struct DrawableImage ship;
+  struct DrawableImage explosion;
+};
 
 const uint8_t spaceship_frame_0[24] = {
   0x01, 0x00, 0x00,
@@ -41,18 +54,19 @@ const uint8_t player_laser_frame_1[2] = { 0xDF, 0xBF };
 const uint8_t player_laser_frame_2[2] = { 0x7F, 0x1F };
 const uint8_t player_laser_frame_3[2] = { 0x9F, 0x3F };
 
+
 const struct Image spaceship_image_0 = {.width = 24, .height = 8, .pixels = spaceship_frame_0};
 const struct Image spaceship_image_1 = {.width = 24, .height = 8, .pixels = spaceship_frame_1};
 const struct Image spaceship_image_2 = {.width = 24, .height = 8, .pixels = spaceship_frame_2};
-static struct DrawableImage spaceship = {.x = UNINITIALIZED, .y = UNINITIALIZED, .state = 0, .images = {&spaceship_image_0, &spaceship_image_1, &spaceship_image_2}};
 
 const struct Image player_laser_image_0 = {.width = 8, .height = 2, .pixels = player_laser_frame_0};
 const struct Image player_laser_image_1 = {.width = 8, .height = 2, .pixels = player_laser_frame_1};
 const struct Image player_laser_image_2 = {.width = 8, .height = 2, .pixels = player_laser_frame_2};
 const struct Image player_laser_image_3 = {.width = 8, .height = 2, .pixels = player_laser_frame_3};
-static struct DrawableImage player_laser = {.x = UNINITIALIZED, .y = UNINITIALIZED, .state = 0, .images = {&player_laser_image_0, &player_laser_image_1, &player_laser_image_2, &player_laser_image_3}};
 
-static 
+
+static struct PlayerShip  player_ship;
+static struct PlayerLaser player_lasers[MAX_PLAYER_LASERS];
 
 signed char check_collisions() {
 
@@ -60,6 +74,22 @@ signed char check_collisions() {
 }
 
 signed char game_tick(struct InvaderCommands commands) {
+  
+  return 0;
+}
 
+signed char game_setup() {
+  /* Initialize player ship */
+  player_ship = (struct PlayerShip){.alive = TRUE, .ship = (struct DrawableImage){.x = UNINITIALIZED, .y = UNINITIALIZED, .state = 0, .images = {&spaceship_image_0, &spaceship_image_1, &spaceship_image_2}} };
+  
+  /* Initialize player laser structs */
+  for (int i = 0; i < MAX_PLAYER_LASERS; i++) {
+    player_lasers[i].active = FALSE;
+    // struct DrawableImage player_laser_init =
+    player_lasers[i].laser = (struct DrawableImage){.x = UNINITIALIZED, .y = UNINITIALIZED, .state = 0,
+      .images = {&player_laser_image_0, &player_laser_image_1, &player_laser_image_2, &player_laser_image_3}};
+  }
+
+  
   return 0;
 }
