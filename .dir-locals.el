2ed2b672307abf29a05d16a1b0b9d0346a3af44a
RJSent <richard.j.sent@tutamail.com>
2021-04-20 17:33:57 -0400

    added dir-locals fixme

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,8 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((eval . (setq compile-command
+		       ;; FIXME: only set to this if it's not already
+		       ;; set. Don't want to clear command
 		       (format "cd %s && make"
 			       (projectile-project-root))))))
  (c-mode . ((eval . (setq flycheck-clang-args (list "-nostdinc")))
